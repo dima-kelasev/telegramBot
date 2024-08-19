c6b9dfa637f6845d33df1a157383993bb18426b2
dima-kelasev <kel-da@mail.ru>
2024-08-19 23:18:38 +0300

    add api for forecast

---
+++ const/index.js
@@ -0,0 +1,6 @@
+
+const HEADER = {
+    'X-Yandex-Weather-Key': process.env.YANDEX_API_KEY
+};
+
+module.exports = { HEADER };
---
+++ helper/get-smail.js
@@ -0,0 +1,16 @@
+const getSmile = (temp) => {
+    if(temp > 10 && temp < 20) {
+        return '🌤'
+    } else if(temp > 20 && temp < 30) {
+        return '☀️'
+    } else if(temp > 30) {
+        return '🔥'
+    } else if(temp < 10) {
+        return '❄️'
+    } else {
+        '⭐️'
+    }
+
+}
+
+module.exports = { getSmile }
